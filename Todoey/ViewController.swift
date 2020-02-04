3a0850a351a3cbffc96223c767bb7e418767879a
ali ka <ninja.armin@gmail.com>
2020-02-04 20:45:13 +0330

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ali Ka on 11/15/1398 AP.
+//  Copyright © 1398 AP Ali Ka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
